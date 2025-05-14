dcc33c299c378f05dce10212430ace4f8686b769
vijaysan7 <vijaysan@students.zhaw.ch>
2025-05-14 14:50:56 +0200

    first commit

---
+++ src/lib/components/MovieCard.svelte
@@ -0,0 +1,35 @@
+<script>
+  let { movie } = $props();
+</script>
+
+<div class="movie-card">
+  <div>
+    <img class="img-fluid" src={movie.poster} alt="" />
+  </div>
+  <div class="details">
+    <div class="title">
+      <a href={"/movies/" + movie._id}>{movie.title}</a>
+    </div>
+    <div>
+      Jahr: {movie.year}
+    </div>
+    <div>
+      Dauer: {movie.length}
+    </div>
+  </div>
+</div>
+
+<style>
+  .movie-card {
+    border: 1px solid #555;
+    height: 100%;
+    background-color: #444;
+    color: white;
+  }
+  .details {
+    padding: 0.5em;
+  }
+  .title {
+    font-weight: bold;
+  }
+</style>
---
+++ src/routes/movies/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import MovieCard from "$lib/components/MovieCard.svelte";
+  let { data } = $props();
+</script>
+
+<p><i>Daten und Bilder generiert mit ChatGPT und DALL-E</i></p>
+<div>
+  <a href="/movies/create" class="btn btn-primary">Add New Movie</a>
+</div>
+<div class="row mt-3">
+  {#each data.movies as movie}
+    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
+      <MovieCard {movie}></MovieCard>
+    </div>
+  {/each}
+</div>
